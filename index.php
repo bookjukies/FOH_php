b6c50f5ca9e617a80bf36fea1a7d95d3758cf4d5
Bongani <mkhumbie34@gmail.com>
2022-08-29 14:05:57 +0200

    mobile test

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./style.css" />
+    <title>FOH RSA</title>
+  </head>
+  <body>
+    <div class="container">
+      <!-- header component -->
+      <header>
+        <div class="logo-container">
+          <img src="./asset/images/logo.png" alt="logo" class="logo" />
+        </div>
+
+        <nav class="nav">
+          <ul>
+            <li><a href="#music">Music</a></li>
+            <!-- <li><a href="#about">About</a></li> -->
+            <li><a href="#beats">Beats</a></li>
+            <li><a href="#video">Music Video</a></li>
+          </ul>
+        </nav>
+
+        <a
+          href="https://api.whatsapp.com/send?phone=%2B27764398902&fbclid=IwAR2lCXm706C-H6dPrX9jszbFsNHpWEGWb0fLSMCdfk8m6F04wsVviTn0pLk"
+          ><div class="book"><button>BOOK NOW!</button></div></a
+        >
+      </header>
+
+      <div class="main">
+        <div class="left">
+          <h1>LOVE <span>IS TREE OF LIFE</span></h1>
+          <img src="./asset/images/play.png" alt="play button" />
+        </div>
+        <div class="right">
+          <span class="open"> "</span>
+          I met melissa at college in 2020, she was struggling with her
+          registration then i decided to assist her. we started to hag out, i
+          still remember the moment the was a strike we chilled together and
+          that where i even noticed her beautiful eyes. honestly she was a very
+          complicated person because i thought she felt something too, becacuse
+          we were so connected.<span class="close">"</span>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
